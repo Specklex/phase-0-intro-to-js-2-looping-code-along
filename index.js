ebea66a0d3691a2aa9f938d68df2dc41c4a8695f
Jaden Lund <lundjaden@gmail.com>
2022-07-01 22:22:24 -0400

    commit tree 3e3a47b9c4b34b1e7f152060f4bc8cacbe7d8c4f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, gifts){
+    const messages = [];
+    for(let i = 0; i < names.length; i++){
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${gifts} gift!`);
+    }
+    return messages;
+}
+
+function countDown(){
+   let number = 10;
+    while (number >= 0 ){
+        console.log(number)
+        number--;
+    }
+    
+}
